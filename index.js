57816a0a77f2c75f0bebea1d0713816919cb14a1
marlon475 <marlon.cruz-estevez25@compscihigh.org>
2025-05-13 19:10:10 +0000

    Progress Point #1

--- index.js
+++ index.js
@@ -1,1 +1,75 @@
 
+const mongoose = require("mongoose");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.use(express.json());
+
+app.set("view engine", "ejs");
+
+app.use((req, res, next) => {
+    console.log(`${req.method}: ${req.path}`);
+    next();
+});
+
+const studentSchema = new mongoose.Schema(
+    {
+        name: { type: String },
+        grade: { type: Number, default: 0 },
+        numberOfF:{type: Number}
+    }
+);
+
+const Student = mongoose.model("Student", studentSchema, "Students");
+
+app.get("/", async (req, res) => {
+    const students = await Student.find({});
+    res.render("student.ejs", { students });
+});
+
+// Task 1: Write the DELETE route to delete by ID
+// Complete tasks 2 & 3 in alma.ejs and script.js
+app.delete("/delete/:_id", async (req, res) => {
+    const response = await Student.findOneAndDelete({ _id: req.params._id })
+    res.json(response);    
+});
+
+// Task 4: Write the PATCH route to update by ID 
+app.patch("/student/:_id", async (req, res) => {
+    const response = await Student.findOneAndUpdate({ _id: req.params._id }, 
+    req.body, {new: true})
+    res.json(response);
+    });
+    
+
+
+async function prepopulateDb() {
+    try {
+        // Feel free to change the names you want to populate
+        await Student.insertMany([
+            { name: "Enter name here" }
+        ]);
+
+        console.log('Students added successfully!');
+    } catch (err) {
+        console.error('Error prepopulating database:', err);
+    }
+}
+
+async function startServer() {
+    // Add your SRV string, make sure that the database is called SE12
+    await mongoose.connect("mongodb+srv://SE12:CSH2025@cluster0.xfcbvkb.mongodb.net/SE12?retryWrites=true&w=majority&appName=Cluster0");
+
+
+    
+ prepopulateDb()
+
+    app.listen(3000, () => {
+        console.log(`Server running.`);
+    });
+}
+
+startServer();
---
+++ public/script.js
@@ -0,0 +1,24 @@
+async function deleteStudent(id) {
+    await fetch('/delete/' + id, {method: 'DELETE'});
+    window.location.href = "/"
+   }
+   
+
+// Task 6: Write the update student function using ID
+async function updateStudent(e, id) {
+    e.preventDefault();
+   
+    const formData = new FormData(e.target);
+    const formObject = Object.fromEntries(formData.entries());
+   
+    await fetch('/student/' + id, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formObject)
+    });
+   
+    window.location.href = '/'
+   }
+   
+
+
